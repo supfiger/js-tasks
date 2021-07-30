781c8df8d0be47eeb198064d0a55a52525da8b8f
Danil Zinchenko <danilzin777@gmail.com>
2021-07-30 07:32:18 +0300

    update

--- js/3_task.js
+++ js/3_task.js
@@ -5,7 +5,7 @@ function createRandomIntegerArrayInRange(min, max) {
     .sort(() => Math.random() - 0.5);
 }
 
-function findPairWithSpecificSums(arr, specificSum) {
+function findPairWithSpecificSums2(arr, specificSum) {
   return arr.reduce((prev, value, index) => {
     const currIndex = index;
     const currValue = value;
@@ -54,8 +54,31 @@ function findPairWithSpecificSums(arr, specificSum) {
   }, {});
 }
 
-const arr = createRandomIntegerArrayInRange(1, 5);
-const sum = findPairWithSpecificSums(arr, 7);
+const compare = ({ array, initialIndex, nextIndex, givenSum, pair, prev }) =>
+  (nextIndex = (nextIndex || initialIndex) + 1) === array.length
+    ? pair || prev
+    : (array[initialIndex] + array[nextIndex] === givenSum &&
+        (initialIndex + nextIndex < pair?.sumOfIndexes
+          ? (pair = {
+              num1: { value: array[initialIndex], index: initialIndex },
+              num2: { value: array[nextIndex], index: nextIndex },
+              sumOfIndexes: initialIndex + nextIndex,
+            })
+          : pair),
+      compare({ array, initialIndex, nextIndex, givenSum, pair, prev }));
 
+const findPairWithGivenSums = (arr, givenSum) => {
+  return arr.reduce((prev, _, index, array) => {
+    const curr = compare({ array, initialIndex: index, givenSum, prev });
+    const result = prev.sumOfIndexes < curr.sumOfIndexes ? prev : curr;
+
+    return { ...prev, ...result };
+  }, {});
+};
+
+const arr = createRandomIntegerArrayInRange(1, 10);
+const sum = findPairWithGivenSums(arr, 10);
+
+console.log("------------");
 console.log("arr:", arr);
 console.log("sum:", sum);
